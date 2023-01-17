4a4e1f3b13aab5bde9cab6404021192fe68efc39
IgorMarokhin <lawyer.mii222@gmail.com>
2023-01-17 20:19:46 +0300

    Семинар 5. Добавлено предварительное решение задачи 37.

---
+++ Seminar_005_Task_37/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 37. Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+// Результат запишите в новом массиве.
+
+int[] array2 = GetNewArray(array);
+Console.WriteLine(String.Join(" ", array2));
+
+/////////////////////////////////////////////
+
+int GetNewArray(int[] arr)
+{
+    int size = 0;
+    if(arr.Length % 2 == 1)
+        size = arr.Length / 2 + 1;
+    else
+        size = arr.Length / 2;
+    
+    int result = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        if((arr.Length % 2 == 1) && (i == size - 1))
+            result[i] = arr[i];
+        else
+            result[i] = arr[i] * array[array.Length - i - 1];
+    }
+    result result;
+}
