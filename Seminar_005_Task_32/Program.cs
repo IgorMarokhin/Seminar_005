97f1940498cb35dc0d341dcee4db13d9d095ab50
IgorMarokhin <lawyer.mii222@gmail.com>
2023-01-17 17:05:41 +0300

    Семинар 5. Добавлено решение задачи 32.

---
+++ Seminar_005_Task_32/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 32. Напишите программу замены элементов массива: положительные элементы
+// замените на соответствующие отрицательные, и наоборот.
+
+int[] array = GetRandomArray(8, 0, 100);
+Console.Write($"[{String.Join(", ", array)}]");
+ChangeArray(array);
+Console.WriteLine($" -> [{String.Join(", ", array)}]");
+
+/////////////////////////////////////////////
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    var rnd = new Random();
+    for(int i = 0; i < size; i++)
+    {
+        result[i] = rnd.Next(minValue, maxValue);
+    }
+    return result;
+}
+
+void ChangeArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = - array[i];
+}
