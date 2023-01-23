f432152ed89602b439876f2fd1392876a20286b0
IgorMarokhin <lawyer.mii222@gmail.com>
2023-01-23 11:35:09 +0400

    Семинар 5. Домашнее задание.  Добавлено решение задачи 34.

---
+++ Seminar_005_HW_Task_34/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 34. Задайте массив заполненный случайными положительными трехзначными числами.
+// Напишите программу, которая покажет количество четных чисел в массиве.
+
+int[] array = GetRandomArray(6, 100, 999);
+Console.Write($"[{String.Join(", ", array)}]");
+int count = CountEvenNumber(array);
+Console.WriteLine($" -> [{String.Join(", ", count)}]");
+
+/////////////////////////////////////////////
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    var rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = rnd.Next(minValue, maxValue);
+    }
+    return result;
+}
+
+int CountEvenNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 != 1)
+            count++;
+    }
+    return count;
+}
