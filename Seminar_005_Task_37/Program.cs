87a96dba91900dcee31ad5d4792dc199980bf0a3
IgorMarokhin <lawyer.mii222@gmail.com>
2023-01-23 11:00:24 +0400

    Семинар 5. Добавлено предварительное решение задачи 37.

--- Seminar_005_Task_37/Program.cs
+++ Seminar_005_Task_37/Program.cs
@@ -2,26 +2,23 @@
 // Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 // Результат запишите в новом массиве.
 
-int[] array2 = GetNewArray(array);
-Console.WriteLine(String.Join(" ", array2));
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int amount = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[amount];
+InputArray(array);
+int[] newArray = ReleaseArray(array);
+Console.WriteLine($"Исходный массив: [{string.Join(", ", array)}] -> [{string.Join(", ", newArray)}]");
 
-/////////////////////////////////////////////
-
-int GetNewArray(int[] arr)
+////////////////////////////////////////////////
+void InputArray(int[] array)
 {
-    int size = 0;
-    if(arr.Length % 2 == 1)
-        size = arr.Length / 2 + 1;
-    else
-        size = arr.Length / 2;
-    
-    int result = new int[size];
-    for(int i = 0; i < size; i++)
-    {
-        if((arr.Length % 2 == 1) && (i == size - 1))
-            result[i] = arr[i];
-        else
-            result[i] = arr[i] * array[array.Length - i - 1];
-    }
-    result result;
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(0, 11);
+}
+int[] ReleaseArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2 + array.Length % 2; i++)
+        Console.Write($"{array[i] * array[array.Length - i - 1]} ");
+    return array;
 }
